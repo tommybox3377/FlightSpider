c2b975f817fe13af468408eac2e97a87d9fe6887
twmaryniak <twmaryniak@yahoo.com>
2019-11-20 19:43:53 -0500

    Initial commit

---
+++ Travel_Vars.py
@@ -0,0 +1,28 @@
+travel_origin = [
+    "EWR",
+    "ABE",
+    "PHL"
+]
+
+travel_destination = [
+    "YYC",
+    "YYT",
+    "YDF",
+    "ANC"
+]
+
+departure_dates = [
+    "2020-09-10",
+    "2020-10-11"
+]
+
+return_dates = [
+    "2020-09-12",
+    "2020-10-13"
+]
+
+adults = 2
+
+max_stops = 2
+
+max_duration = 720
